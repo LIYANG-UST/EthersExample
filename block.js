e58eeae4a97589a0544a0235374069f3363438b7
liyang <42539774+LIYANG-UST@users.noreply.github.com>
2022-10-14 13:36:39 +0800

    feat: add blockjs

---
+++ block.js
@@ -0,0 +1,8 @@
+const { ethers } = require("ethers");
+
+(async () => {
+    const provider = new ethers.providers.AlchemyProvider();
+
+    const block = await provider.getBlock(14292113);
+    console.log("block: ", block);
+})();
